4e23e1dbff01707b540a7d2e3161682bde991d73
yangyuan <yangyuan@weidian.com>
2015-12-12 13:25:40 +0800

    finish

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+/*
+ * grunt-concat-require-config
+ * https://github.com/iamyy/grunt-concat-require-config
+ *
+ * Copyright (c) 2015 yangyuan
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    concat_require_config: {
+
+      build : {
+
+        options : {
+          map : {
+            "Common" : [ "Backbone", "UI" ]
+          }
+        },
+        dest : "testConfig.js"
+      }
+      
+      // default_options: {
+      //   options: {
+      //   },
+      //   files: {
+      //     'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+      //   }
+      // },
+      // custom_options: {
+      //   options: {
+      //     separator: ': ',
+      //     punctuation: ' !!!'
+      //   },
+      //   files: {
+      //     'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+      //   }
+      // }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'concat_require_config']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', [ 'test']);
+
+};
---
+++ tasks/concat_require_config.js
@@ -0,0 +1,62 @@
+/*
+ * grunt-concat-require-config
+ * https://github.com/iamyy/grunt-concat-require-config
+ *
+ * Copyright (c) 2015 yangyuan
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('concat_require_config', 'Handle the config file of require in your project which using grunt-contrib-requirejs for optimizing.', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      // 配置模块名称与对打包合并后的模块的应路径
+      // 比如 Backbone 和 Zepto 通过 grunt-contrib-require 合并为 Common 模块
+      // 那么 map : { Common : [ "Backbone", "Zepto" ], ... }
+      map : {}
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // 读取 requirejs 项目 dest 目录中的 requirejs 配置文件( config.js )
+      var requireConfigFile = grunt.file.read( file.dest );
+
+      var bIndex = requireConfigFile.indexOf( '{' ),
+          eIndex = requireConfigFile.lastIndexOf( '}' );
+
+      var configJSONStr = requireConfigFile.substr( bIndex, eIndex - bIndex + 1 ),
+          configJSON = eval("("+configJSONStr+")");
+
+      // 处理 paths
+      for( var module in configJSON.paths ){
+          for( var key in options.map ){
+            if( options.map[ key ].indexOf( module ) != -1 ){
+               configJSON.paths[ module ] = key;
+               continue;
+            }
+          }
+      }
+
+      // 处理 shim
+      for( var module in configJSON.shim ){
+        for( var key in options.map ){
+            if( options.map[ key ].indexOf( module ) != -1 ){
+               delete configJSON.shim[ module ];
+               continue;
+            }
+          }
+      }
+
+      grunt.file.write( file.dest, 'requirejs.config(' +  JSON.stringify( configJSON ) + ');' );
+     
+    });
+  });
+
+};
